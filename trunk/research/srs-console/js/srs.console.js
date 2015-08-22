97aab42f32d9f22fc21ea99ab9ab105c9275d61b
winlin <winlin@vip.126.com>
2015-08-22 19:47:23 +0800

    add nav and connect to srs.

--- trunk/research/srs-console/js/srs.console.js
+++ trunk/research/srs-console/js/srs.console.js
@@ -3,21 +3,18 @@ var scApp = angular.module("scApp", ["ngRoute", "ngResource",
 ]);
 
 scApp.config(["$routeProvider", function($routeProvider){
-    $routeProvider.otherwise({redirectTo:"/summaries"})
+    $routeProvider.otherwise({redirectTo:"/connect"})
+        .when("/connect", {templateUrl:"views/connect.html", controller:"CSCConnect"})
         .when("/summaries", {templateUrl:"views/summary.html", controller:"CSCSummary"});
-}])
-// config the http interceptor.
-.config(['$httpProvider', function($httpProvider){
-    $httpProvider.interceptors.push('MHttpInterceptor');
 }]);
 
-scApp.controller("CSCMain", ["$scope", "$interval", "$sc_utility", function($scope, $interval, $sc_utility){
+scApp.controller("CSCMain", ["$scope", "$interval", "$location", "$sc_utility", "$sc_server", function($scope, $interval, $location, $sc_utility, $sc_server){
     $scope.logs = [];
     // remove expired alert.
     $interval(function(){
         for (var i = 0; i < $scope.logs.length; i++) {
             var log = $scope.logs[i];
-            if (log.create + 5000 < new Date().getTime()) {
+            if (log.create + 3000 < new Date().getTime()) {
                 $scope.logs.splice(i, 1);
                 break;
             }
@@ -28,6 +25,10 @@ scApp.controller("CSCMain", ["$scope", "$interval", "$sc_utility", function($sco
         var log = {
             level:level, msg:msg, create:new Date().getTime()
         };
+        // only show 3 msgs.
+        while ($scope.logs.length > 2) {
+            $scope.logs.splice($scope.logs.length - 1, 1);
+        }
         $scope.logs.push(log);
     });
 
@@ -36,11 +37,32 @@ scApp.controller("CSCMain", ["$scope", "$interval", "$sc_utility", function($sco
         // TODO: FIXME: parse the error.
         $sc_utility.log("warn", response);
     });
+
+    $sc_server.init($location);
+    //$sc_utility.log("trace", "set baseurl to " + $sc_server.baseurl());
 }]);
 
-scApp.controller("CSCSummary", ["$scope", "MSCSummary", "$sc_utility", function($scope, MSCSummary, $sc_utility){
-    MSCSummary.summaries_get(function(data){
-        $sc_utility.log("trace", "Retrieve summary from SRS ok.")
+scApp.controller("CSCConnect", ["$scope", "$location", "MSCApi", "$sc_utility", "$sc_nav", "$sc_server", function($scope, $location, MSCApi, $sc_utility, $sc_nav, $sc_server){
+    $sc_nav.in_control();
+    $scope.server = {
+        ip: $sc_server.host,
+        port: $sc_server.port
+    };
+    $scope.connect = function(){
+        $sc_server.host = $scope.server.ip;
+        $sc_server.port = $scope.server.port;
+
+        MSCApi.versions_get(function(data){
+            $sc_utility.log("trace", "连接到SRS" + $scope.server.ip + "成功, SRS/" + data.data.version);
+            $sc_nav.go_summary($location);
+        });
+    };
+}]);
+scApp.controller("CSCSummary", ["$scope", "MSCApi", "$sc_utility", "$sc_nav", function($scope, MSCApi, $sc_utility, $sc_nav){
+    $sc_nav.in_summary();
+
+    MSCApi.summaries_get(function(data){
+        $sc_utility.log("trace", "Retrieve summary from SRS ok.");
         $scope.server = data.data.self;
     });
 }]);
@@ -51,12 +73,66 @@ scApp.filter("sc_filter_log_level", function(){
     };
 });
 
-scApp.factory("MSCSummary", ["$resource", function($resource){
-    return $resource("/api/v1/summaries", {}, {
-        summaries_get: {method:"GET"}
-    });
+scApp.filter("sc_filter_nav_active", ["$sc_nav", function($sc_nav){
+    return function(v){
+        return $sc_nav.is_selected(v)? "active":"";
+    };
 }]);
 
+scApp.factory("MSCApi", ["$http", "$sc_server", function($http, $sc_server){
+    return {
+        versions_get: function(success) {
+            $http.jsonp($sc_server.jsonp("/api/v1/versions")).success(success);
+        },
+        summaries_get: function(success) {
+            $http.jsonp($sc_server.jsonp("/api/v1/summaries")).success(success);
+        }
+    };
+}]);
+
+// the sc nav is the nevigator
+scApp.provider("$sc_nav", function(){
+    this.$get = function(){
+        return {
+            selected: null,
+            in_control: function(){
+                this.selected = "/console";
+            },
+            in_summary: function(){
+                this.selected = "/summaries";
+            },
+            go_summary: function($location){
+                $location.path("/summaries");
+            },
+            is_selected: function(v){
+                return v == this.selected;
+            }
+        };
+    };
+});
+
+// the sc server is the server we connected to.
+scApp.provider("$sc_server", function(){
+    this.$get = function(){
+        return {
+            schema: "http",
+            host: null,
+            port: 1985,
+            baseurl: function(){
+                return this.schema + "://" + this.host + (this.port == 80? "": ":" + this.port);
+            },
+            jsonp: function(url){
+                return this.baseurl() + url + "?callback=JSON_CALLBACK";
+            },
+            init: function($location) {
+                this.host = $location.host();
+                this.port = $location.port();
+            }
+        };
+    };
+});
+
+// the sc utility is a set of helper utilities.
 scApp.provider("$sc_utility", function(){
     this.$get = ["$rootScope", function($rootScope){
         return {
@@ -69,6 +145,13 @@ scApp.provider("$sc_utility", function(){
         };
     }];
 });
+
+// config the http interceptor.
+scApp.config(['$httpProvider', function($httpProvider){
+    $httpProvider.interceptors.push('MHttpInterceptor');
+}]);
+
+// the http interceptor.
 scApp.factory('MHttpInterceptor', ["$q", "$sc_utility", function($q, $sc_utility){
     // register the interceptor as a service
     // @see: https://code.angularjs.org/1.2.0-rc.3/docs/api/ng.$http
